175dc2a02de680b20615161a336aef4b439e7001
Carmen Morado <cmora171@fiu.edu>
2021-07-21 16:06:35 -0400

    (CS193p - Developing Apps for iOS - Stanford University) Lecture 2: Learning more about SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,29 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗","🚌","🚑","🚜","🛴","🚲","🚔","🛺","🚘","🚃","🚄","🚠","🚅","✈️","🚀","🛸","🚁","🛶","🚤","🚢","🛳","🚓","🚛","🛵"]
+    @State var emojiCount = 20
+    
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns:[GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
             }
             .padding(.horizontal)
-            .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+            .font(.largeTitle)
+        }
+        .padding(.horizontal)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+                }
+                label: {
+                Image(systemName: "minus.circle")
+                }
+    }
+    
+    var add: some View {
+    Button {
+        if emojiCount < emojis.count {
+            emojiCount += 1
+        }
+           }
+           label: {
+           Image(systemName: "plus.circle")
+           }
     }
 }
 
 struct CardView: View {
+    var content: String
     @State var isFaceUp: Bool = true
     
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius:20)
             if isFaceUp {
-                shape.fill().frame(width: 60, height: 750).foregroundColor(.white)
-                shape.stroke(lineWidth:3)
-                Text("✈️")
-                    .font(.largeTitle)
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth:3)
+                Text(content).font(.largeTitle)
                 
             }
             
