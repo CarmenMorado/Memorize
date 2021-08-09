4d6e3a3780dc31116eaa7855fee695650423720e
Carmen Morado <cmora171@fiu.edu>
2021-08-09 15:47:46 -0400

    The Model and ViewModel have been completed for Memorize.

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,25 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Carmen Morado on 8/8/21.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    
+    static let emojis = ["🚗","🚌","🚑","🚜","🛴","🚲","🚔","🛺","🚘","🚃","🚄","🚠","🚅","✈️","🚀","🛸","🚁","🛶","🚤","🚢","🛳","🚓","🚛","🛵"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4)  { pairIndex in
+            EmojiMemoryGame.emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
